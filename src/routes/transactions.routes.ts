f1bf5646af5f51b29834483b76304bef5c0bb47a
lucianakeiko <lucianakeiko@gmail.com>
2020-06-08 22:50:37 -0300

    falta categoria

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -9,18 +10,37 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   // TODO
+  const transactionsRespository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRespository.find();
+  const balance = await transactionsRespository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
   // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
   // TODO
+  return response.json({ ok: true });
 });
 
 export default transactionsRouter;
